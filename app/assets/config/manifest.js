2e01fbaaba120a0ede9e55bdc86edfcffe58552d
Nasserfq <nonq70007@gmail.com>
2021-09-21 10:40:41 +0300

    Add to manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,13 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+{
+    "name": "Appscope",
+    "display": "standalone",
+    "icons": [{
+       "src": "icons/icon-192.png",
+       "sizes": "192x192"},
+    {
+       "src": "icons/icon-512.png",
+       "sizes": "512x512"}
+    ]
+ }
